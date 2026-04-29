39da8c91bd62fd731d42fc2b09fe1686217c9f4b
Lauzalaiah <82446542+Lauzalaiah@users.noreply.github.com>
2026-04-29 10:53:27 +0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -3,10 +3,7 @@ import { Hero } from "@/components/hero"
 
 export default function Home() {
   return (
-    <div
-      className="relative min-h-screen bg-cover bg-center bg-repeat"
-      style={{ backgroundImage: "url('/images/dollar-bg-bright.jpg')" }}
-    >
+   <div className="bg-black min-h-screen">
       {/* overlay sombre */}
       <div className="absolute inset-0 z-0 bg-black/60" />
 
