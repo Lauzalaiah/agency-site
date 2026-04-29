7f59cefa3b2e68cc99abfc9dad36af3ffeeccf84
Lauzalaiah <82446542+Lauzalaiah@users.noreply.github.com>
2026-04-29 10:57:40 +0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,3 @@
-import { Services } from "@/components/services"
-import { Hero } from "@/components/hero"
-
 export default function Home() {
-  return (
-   <div className="bg-black min-h-screen">
-      {/* overlay sombre */}
-      <div className="absolute inset-0 z-0 bg-black/60" />
-
-      <div className="relative z-10">
-        <Hero />
-        <Services />
-      </div>
-    </div>
-  )
+  return <div className="bg-black min-h-screen text-white">TEST</div>
 }
