66508d30cb016813fb08b71cf7ec0282b9c2b2e8
Lauzalaiah <82446542+Lauzalaiah@users.noreply.github.com>
2026-04-30 20:27:40 +0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,14 +18,15 @@ export const metadata = {
   title: 'Leo OFM Elite',
   description: 'We scale creators to $10k+/month',
   icons: {
-    icon: [
-      { url: '/favicon.ico' },
-    ],
-    shortcut: ['/favicon.ico'],
-    apple: [
-      { url: '/apple-icon.png', sizes: '180x180', type: 'image/png' },
-    ],
-  },
+  icon: [
+    { url: '/favicon.ico' },
+    { url: '/android-chrome.png', sizes: '512x512', type: 'image/png' },
+  ],
+  shortcut: ['/favicon.ico'],
+  apple: [
+    { url: '/apple-icon.png', sizes: '180x180', type: 'image/png' },
+  ],
+}
 }
 
 export const viewport: Viewport = {
