8078b85c3ab6dab6a9572bc9867ec25024bde77c
Lauzalaiah <82446542+Lauzalaiah@users.noreply.github.com>
2026-05-03 17:39:07 +0300

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,25 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "Content-Security-Policy",
+            value: `
+              default-src 'self';
+              script-src 'self' 'unsafe-inline' 'unsafe-eval' https://challenges.cloudflare.com;
+              frame-src https://challenges.cloudflare.com;
+              connect-src 'self' https://challenges.cloudflare.com;
+              img-src 'self' data:;
+              style-src 'self' 'unsafe-inline';
+            `.replace(/\n/g, ""),
+          },
+        ],
+      },
+    ]
+  },
+}
+
+module.exports = nextConfig
